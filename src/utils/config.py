41c7d0138ab0c8e00ff956a99c523a269d0a57e2
essteer <essteer@pm.me>
2024-04-24 18:32:56 +0800

    docs: Update readme and model choice

--- src/utils/config.py
+++ src/utils/config.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 
 ENDPOINT = "https://api.together.xyz/v1/completions"
-IMAGE_MODEL = "stabilityai/stable-diffusion-xl-base-1.0"
+# IMAGE_MODEL = "stabilityai/stable-diffusion-xl-base-1.0"
+IMAGE_MODEL = "stabilityai/stable-diffusion-2-1"
 NEGATIVE_PROMPT = "Nudity, Violence, Gore, Blood, Sex, NSFW"
 HEIGHT = 1024
 WIDTH = 1024
